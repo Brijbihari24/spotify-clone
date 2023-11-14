e02ca789866d089e2f0e22055380ed318330461a
Brijbihari <ajitawadhiya@gmail.com>
2023-11-14 18:31:25 +0530

    ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
-import { RxCaretLeft } from "react-icons/rx";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -8,7 +12,7 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
-  //   const router = useRouter();
+  const router = useRouter();
 
   const handleLogout = () => {
     //will handle logout later
@@ -20,7 +24,47 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         `h-fit bg-gradient-to-b from-emerald-800 p-6`,
         className
       )}
-    ></div>
+    >
+      <div
+        className="
+      w-full
+      mb-4
+      flex
+      items-center
+      justify-between
+      "
+      >
+        <div
+          className="
+        hidden
+        md:flex
+        gap-x-2
+        items-center
+        "
+        >
+          <button
+            onClick={() => router.back()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+          >
+            <RxCaretLeft size={35} className="text-white" />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition"
+          >
+            <RxCaretRight size={35} className="text-white" />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-cneter justify-center hover:opacity-75 transition">
+            <HiHome size={20} className="text-black" />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-cneter justify-center hover:opacity-75 transition">
+            <BiSearch size={20} className="text-black" />
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
